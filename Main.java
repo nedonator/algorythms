ea9a98b80d8bd0fe9f37ea26a27050ebb5b3775a
Elena Gorskaya <helena557@mail.ru>
2020-10-20 20:06:21 +0300

    refactor

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
-        for(int i = 0; i < n + 1; i++){
+        for (int i = 0; i < n + 1; i++) {
             int num = sc.nextInt();
-            if(a[num-1]++==1){
+            if (a[num - 1]++ == 1) {
                 System.out.println(num);
             }
         }
